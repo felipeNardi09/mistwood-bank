ce3770e9361ec94aac01255a9a8a56a1d60a195d
Felipe Nardi <felipenardi_92@hotmail.com>
2024-06-25 17:08:03 -0300

    created delete route for accounts

--- src/app/routes/account/account.controller.ts
+++ src/app/routes/account/account.controller.ts
@@ -45,29 +45,66 @@ router.post(
 );
 
 router.get(
-  '/acoount/all-accounts',
+  '/account/all-accounts',
   validateToken,
   catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+    //filter by user
     const accounts = await prisma.account.findMany();
 
-    res.status(200).json(accounts);
+    res.status(200).json({ total: accounts.length, accounts });
   })
 );
 
 router.get(
-  '/branch/:id',
+  '/account/current-user-accounts',
   validateToken,
   catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
-    const branch = await prisma.branch.findUnique({
+    const accounts = await prisma.account.findMany({
       where: {
-        id: req.params.id
+        userId: req.user.id
       }
     });
 
-    if (!branch)
-      return next(new AppError('There is no branch with provided id', 404));
+    res.status(200).json({ total: accounts.length, accounts });
+  })
+);
 
-    res.status(200).json(branch);
+router.delete(
+  '/account/delete/:id',
+  validateToken,
+  catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+    const account = await prisma.account.findUnique({
+      where: {
+        id: req.params.id
+      },
+      select: {
+        id: true,
+        balance: true,
+        cards: true
+      }
+    });
+
+    if (!account)
+      return next(new AppError('There is no account with provided id', 404));
+
+    if (account.balance !== 0)
+      return next(new AppError('Balance must be 0 to delete an account', 400));
+
+    if (account.cards.length)
+      return next(
+        new AppError(
+          'To delete an account you can not have cards registered',
+          400
+        )
+      );
+
+    await prisma.account.delete({
+      where: {
+        id: account.id
+      }
+    });
+
+    return res.status(204).json();
   })
 );
 
